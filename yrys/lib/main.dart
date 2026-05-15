d7e3da9c20db58156501ade555d5783dfc613b5c
Yrysa <yrysesej@gmail.com>
2026-05-15 14:33:16 +0500

    Initial commit

--- yrys/lib/main.dart
+++ yrys/lib/main.dart
@@ -1,121 +1,147 @@
 import 'package:flutter/material.dart';
+import 'summary.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:wiki_reader/data/repositories/random_article_repository.dart';
+import 'package:wiki_reader/data/services/random_article.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: ArticleView())),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class ArticleModel {
+  Future<Summary> getRandomArticle() async {
+    final uri = Uri.https(
+      'ru.wikipedia.com',
+      'api/rest_v1/page/random/summary',
+    );
+    final response = await get(uri);
+    if (response.statusCode != 200) {
+      throw HttpException("Failed to update resourse");
+    }
+    return Summary.fromJson(json.decode(response.body) as Map<String, Object?>);
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class AricleViewModel extends ChangeNotifier {
+  final ArticleModel repository;
+  Summary? summary;
+  Exception? error;
+  bool isLoading = false;
+  AricleViewModel(this.repository) {
+    fetchArticle();
+  }
+  void fetchArticle() async {
+    isLoading = true;
+    notifyListeners();
+    try {
+      summary = await repository.getRandomArticle();
+      error = null;
+    } on HttpException catch (e) {
+      summary = null;
+      error = e;
+    }
+    isLoading = false;
+    notifyListeners();
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class ArticleWidget extends StatelessWidget {
+  final Summary summary;
+  ArticleWidget({super.key, required this.summary});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 10,
+        children: [
+          if (summary.hasImage) Image.network(summary.originalImage!.source),
+          Text(
+            summary.titles.normalized,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          if (summary.description != null)
+            Text(
+              summary.description!,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+          Text(summary.extract),
+        ],
+      ),
+    );
+  }
+}
+
+class ArticlePage extends StatelessWidget {
+  final Summary summary;
+  final VoidCallback nextArticle;
+  ArticlePage({super.key, required this.summary, required this.nextArticle});
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ArticleWidget(summary: summary),
+          ElevatedButton(onPressed: nextArticle, child: Text("Next Article")),
+        ],
+      ),
+    );
+  }
+}
+
+class ArticleView extends StatefulWidget {
+  const ArticleView({super.key});
+  @override
+  State<ArticleView> createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView> {
+  final viewModel = AricleViewModel(ArticleModel());
+  @override
+  void initState() {
+    super.initState();
+    viewModel.fetchArticle();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      //appBar: AppBar(),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ListenableBuilder(
+          listenable: viewModel,
+          builder: (context, child) {
+            return switch ((
+              viewModel.isLoading,
+              viewModel.summary,
+              viewModel.error,
+            )) {
+              (true, _, _) => const CircularProgressIndicator(),
+              (_, _, Exception e) => Text('error $e'),
+              (_, Summary summary, _) => ArticlePage(
+                summary: summary,
+                nextArticle: viewModel.fetchArticle,
+              ),
+              _ => const Text("Something went wrong"),
+            };
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
