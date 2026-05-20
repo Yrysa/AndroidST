3f594596757cd286afb3d9482299d1de586839a4
Yrysa <130750163+Yrysa@users.noreply.github.com>
2026-05-20 15:41:13 +0500

    Update widget test for splash startup

--- yrys/test/widget_test.dart
+++ yrys/test/widget_test.dart
@@ -1,10 +1,15 @@
 // made by Yrysa
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:yrys/app/yrysa_wiki_app.dart';
 
 void main() {
-  testWidgets('Yrysa Wiki Reader запускается', (tester) async {
+  testWidgets('Yrysa Wiki Reader запускается со splash screen', (tester) async {
+    SharedPreferences.setMockInitialValues({});
+
     await tester.pumpWidget(const YrysaWikiApp());
+
     expect(find.text('Yrysa Wiki Reader'), findsOneWidget);
+    expect(find.text('made by Yrysa'), findsOneWidget);
   });
 }
