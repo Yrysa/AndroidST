5fc5f33eaf3027d6b23b004a2d341e255b0799f4
Yrysa <130750163+Yrysa@users.noreply.github.com>
2026-05-20 14:59:00 +0500

    Add image file model

---
+++ yrys/lib/data/models/image_file.dart
@@ -0,0 +1,18 @@
+// made by Yrysa
+class ImageFile {
+  final String source;
+  final int width;
+  final int height;
+
+  const ImageFile({required this.source, required this.width, required this.height});
+
+  factory ImageFile.fromJson(Map<String, Object?> json) {
+    return ImageFile(
+      source: json['source'] as String? ?? '',
+      width: json['width'] as int? ?? 0,
+      height: json['height'] as int? ?? 0,
+    );
+  }
+
+  bool get isValid => source.isNotEmpty;
+}
