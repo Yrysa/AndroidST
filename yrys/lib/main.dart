302d42fe9bb9a0ecaa943ea53b6affc4a0497285
Yrysa <130750163+Yrysa@users.noreply.github.com>
2026-05-20 14:52:29 +0500

    Simplify main entrypoint

--- yrys/lib/main.dart
+++ yrys/lib/main.dart
@@ -1,26 +1,8 @@
 // made by Yrysa
 import 'package:flutter/material.dart';
 
-import 'ui/article_page/article_screen.dart';
+import 'app/yrysa_wiki_app.dart';
 
 void main() {
   runApp(const YrysaWikiApp());
-}
-
-class YrysaWikiApp extends StatelessWidget {
-  const YrysaWikiApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Yrysa Wiki Reader',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.deepPurple,
-        scaffoldBackgroundColor: const Color(0xFFF7F4FF),
-      ),
-      home: const ArticleScreen(),
-    );
-  }
 }
